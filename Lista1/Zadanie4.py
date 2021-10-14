1c3eeb31077136c0aa466689e57a60b7a5bb557b
Paulina <paulinakm@poczta.onet.pl>
2021-10-14 08:29:35 +0200

    halo

---
+++ Lista1/Zadanie4.py
@@ -0,0 +1,8 @@
+import builtins
+dir(builtins)
+help(print)
+#w sumie nie wiem czy te builtins zadziałało, ale help na pewno
+print("Ala ma kota")
+print(2+2)
+print(2**5,"\t",35//2,"\t",35/2,"\t",35%2,"\t") #// zwraca calość / jest normalne a %to reszta
+print(2**5,"\n",35//2,"\n",35/2,"\n",35%2,"\n")
