3ac347a11bcd18b7004e60e73ee122088cb277fe
Paulina <paulinakm@poczta.onet.pl>
2021-10-14 08:37:38 +0200

    PYTHON

--- Lista1/Zadanie5.py
+++ Lista1/Zadanie5.py
@@ -1,4 +1,7 @@
 import math
-print(5//2)
-print(round(5//2))
-print(math.floor(5//2))
+print(13//5)
+print(round(13/5))
+print(math.floor(13/5))
+#round zaokrągla w dół bądz do gory (od/do 0.5 jak normalnie)
+#floor tylko w dol np 0.6 to do 0
+#print daje po prostu calkowita z dzieleina
