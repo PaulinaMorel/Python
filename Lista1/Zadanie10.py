583265788962a999798926369b30129e29c8c099
Paulina <paulinakm@poczta.onet.pl>
2021-10-14 09:27:07 +0200

    PYTHON

--- Lista1/Zadanie10.py
+++ Lista1/Zadanie10.py
@@ -6,10 +6,10 @@ b=1
 z=complex(a,b)
 
 print("Czesc rzeczywista: ")
-print("sin:", cmath.sin(z).real)
-print("cos:", cmath.cos(z).real)
+print("sin:", cmath.sin(z).real) #zeby liczylo czesc rzeczywista sinus
+print("cos:", cmath.cos(z).real) #no i cos
 print("Czesc urojona: ")
-print("sin:", cmath.sin(z).imag)
+print("sin:", cmath.sin(z).imag) #a tu urojony sinus
 print("cos:", cmath.cos(z).imag)
 
 a=cmath.sin(z)**2+cmath.cos(z)**2
